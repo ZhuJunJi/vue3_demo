f2853c922b2ee69142646369d468cfe0ff1912eb
zhujunji <zhujunji90s@gmail.com>
2024-04-23 23:29:31 +0800

    chore: install element-plus @element-plus/icons-vue

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,14 @@
 <template>
   <div>
     <h3>Hello Vue3</h3>
+    <el-button type="primary" :icon="Plus">新增</el-button>
+    <el-button type="primary" :icon="Edit">编辑</el-button>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import {Plus,Edit} from '@element-plus/icons-vue'
+</script>
+
 
 <style scoped></style>
