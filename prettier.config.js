94a5139cf7d6cdc78b1a440500160419876ab465
zhujunji <zhujunji90s@gmail.com>
2024-04-23 23:43:52 +0800

    style: prettier.config.js

---
+++ prettier.config.js
@@ -0,0 +1,19 @@
+export default {
+	tabWidth: 2,
+  // 缩进方式
+	useTabs: true,
+  // 语句结尾是否加分号
+	semi: false,
+  // 单引号
+	singleQuote: true,
+  // 换行长度
+	printWidth: 120,
+  // 箭头函数参数
+	arrowParens: 'always',
+  // 对象花括号内是否加空格
+	bracketSpacing: true,
+  // 换行符
+	endOfLine: 'auto',
+  // vue 文件内 script 和 style 标签缩进
+	vueIndentScriptAndStyle: true,
+}
